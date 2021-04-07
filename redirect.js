560acdc3ecd0c6be0831d1fa0d297597b1dda428
PitchBlackNights <79918522+PitchBlackNights@users.noreply.github.com>
2021-04-07 15:23:34 -0500

    Delete redirect.js

--- redirect.js
+++
@@ -1,7 +0,0 @@
-var working = 1;
-var url = window.location.href;
-function checkStatus() {
-  if (working == 1) {
-    location.href = 'https://pitchblacknights.github.io/under-construction';
-  }
-}
