3f26ef2eb0360f2efa88cb7d0ec48d6efccd5d1e
PitchBlackNights <79918522+PitchBlackNights@users.noreply.github.com>
2021-04-07 14:20:06 -0500

    Create redirect.js

---
+++ redirect.js
@@ -0,0 +1,10 @@
+var working = 0;
+var url = window.location.href;
+function checkStatus() {
+  if (working = 1) {
+    if (url = "https://pitchblacknights.github.io/under-construction") {
+    }else{
+      window.location.replace("https://pitchblacknights.github.io/under-construction");
+    }
+  }
+}
